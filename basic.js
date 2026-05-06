f80751068738105b3a95a989597761117e9d3e29
AnthonyButtillo <112605347+AnthonyButtillo@users.noreply.github.com>
2026-05-06 01:56:51 -0400

    Add files via upload

---
+++ basic.js
@@ -0,0 +1,36 @@
+async function testBTN() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const token = urlParams.get('token');
+
+    const query = new URLSearchParams({
+        accessApiLevel: '0',
+        include: 'attachments',
+        exclude: 'filteredOutRows',
+        columnIds: 'string',
+        filterId: 'string',
+        ifVersionAfter: '0',
+        level: '0',
+        pageSize: '100',
+        page: '1',
+        paperSize: 'LETTER',
+        rowIds: 'string',
+        rowNumbers: 'string',
+        rowsModifiedSince: '2020-01-30T13:25:32-07:00'
+    }).toString();
+
+    const sheetId = '3455901731082116';
+    const resp = await fetch(
+        `https://corsproxy.io/?url=https://api.smartsheet.com/2.0/sheets/${sheetId}?${query}`,
+        {
+            method: 'GET',
+            headers: {
+                Authorization: 'Bearer ' + token,
+                'smartsheet-integration-source': 'AI,SampleOrg,My-AI-Connector-v2',
+                Accept: 'string'
+            }
+        }
+    );
+
+    const data = await resp.text();
+    console.log(data);
+}
